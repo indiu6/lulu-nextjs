262dbbfe227aea013dd07c4ce6f6c434e6ae8fb0
Sean Yang <hewas6@gmail.com>
2021-06-02 23:46:54 -0400

    done nav menu

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import Results from '../components/Results';
 
 export default function Home() {
   return (
@@ -13,10 +14,13 @@ export default function Home() {
 
       <Header />
 
-      {/* Nav */}
       <Nav />
 
-      {/* Results */}
+      <Results />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
 }
