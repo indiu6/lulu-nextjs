f69eebd27a79bbc582b3f0bb0800a1f831016fa6
Sean Yang <hewas6@gmail.com>
2021-06-25 23:16:16 -0400

    add new components

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import Results from '../components/Results';
+import requests from '../utils/requests';
 
-export default function Home() {
+export default function Home({ results }) {
+  // console.log(props);
+
   return (
     <div>
       <Head>
@@ -13,14 +16,24 @@ export default function Home() {
       </Head>
 
       <Header />
-
       <Nav />
-
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`,
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
